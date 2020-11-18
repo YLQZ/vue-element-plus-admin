474646ff1aacdeb4b5212526fbacb17101f3bde7
yuliang <1194979849@qq.com>
2020-11-18 17:38:04 +0800

    up

---
+++ vue.config.js
@@ -0,0 +1,114 @@
+
+import { join } from 'path';
+import { title } from './src/settings.ts';
+
+function resolve(dir) {
+  return join(__dirname, dir);
+}
+
+const name = title || 'vue Element Admin'; // page title
+
+// If your port is set to 80,
+// use administrator privileges to execute the command line.
+// For example, Mac: sudo npm run
+// You can change the port by the following method:
+// port = 9527 npm run dev OR npm run dev --port = 9527
+const port = process.env.port || process.env.npm_config_port || 9527; // dev port
+
+// All configuration item explanations can be find in https://cli.vuejs.org/config/
+export const publicPath = '/';
+export const outputDir = 'dist';
+export const assetsDir = 'static';
+export const lintOnSave = process.env.NODE_ENV === 'development';
+export const productionSourceMap = false;
+export const devServer = {
+  port,
+  open: true,
+  overlay: {
+    warnings: false,
+    errors: true
+  },
+  before: require('./mock/mock-server.js')
+};
+export const configureWebpack = {
+  // provide the app's title in webpack's name field, so that
+  // it can be accessed in index.html to inject the correct title.
+  name,
+  resolve: {
+    alias: {
+      '@': resolve('src')
+    }
+  }
+};
+export function chainWebpack(config) {
+  // it can improve the speed of the first screen, it is recommended to turn on preload
+  // it can improve the speed of the first screen, it is recommended to turn on preload
+  config.plugin('preload').tap(() => [
+    {
+      rel: 'preload',
+      // to ignore runtime.js
+      // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
+      fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
+      include: 'initial'
+    }
+  ]);
+
+  // when there are many pages, it will cause too many meaningless requests
+  config.plugins.delete('prefetch');
+
+  // set svg-sprite-loader
+  config.module
+    .rule('svg')
+    .exclude.add(resolve('src/icons'))
+    .end();
+  config.module
+    .rule('icons')
+    .test(/\.svg$/)
+    .include.add(resolve('src/icons'))
+    .end()
+    .use('svg-sprite-loader')
+    .loader('svg-sprite-loader')
+    .options({
+      symbolId: 'icon-[name]'
+    })
+    .end();
+
+  config
+    .when(process.env.NODE_ENV !== 'development',
+      (config) => {
+        config
+          .plugin('ScriptExtHtmlWebpackPlugin')
+          .after('html')
+          .use('script-ext-html-webpack-plugin', [{
+            // `runtime` must same as runtimeChunk name. default is `runtime`
+            inline: /runtime\..*\.js$/
+          }])
+          .end();
+        config
+          .optimization.splitChunks({
+            chunks: 'all',
+            cacheGroups: {
+              libs: {
+                name: 'chunk-libs',
+                test: /[\\/]node_modules[\\/]/,
+                priority: 10,
+                chunks: 'initial' // only package third parties that are initially dependent
+              },
+              elementUI: {
+                name: 'chunk-elementUI',
+                priority: 20,
+                test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+              },
+              commons: {
+                name: 'chunk-commons',
+                test: resolve('src/components'),
+                minChunks: 3,
+                priority: 5,
+                reuseExistingChunk: true
+              }
+            }
+          });
+        // https:// webpack.js.org/configuration/optimization/#optimizationruntimechunk
+        config.optimization.runtimeChunk('single');
+      });
+}
